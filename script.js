c3a84b4eca0458eb93899298e83cbbbece0ad1c6
Preston Kwei <prestonkwei@gmail.com>
2024-02-13 19:50:22 -0800

    1.0.2: Visual Updates

--- script.js
+++ script.js
@@ -3,5 +3,4 @@ const data = await fetch("/static.json").then((x) => x.json())
 
 let version = data.version
 
-document.getElementById("static").innerHTML =
-  'Version ' + version /**'·' */
+document.getElementById("static").innerHTML = 'Version ' + version /**'·' */
