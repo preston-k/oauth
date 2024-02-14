64ac9f0a5ae6dbb074da78f3feb0a8200531478b
Preston Kwei <prestonkwei@gmail.com>
2024-02-13 20:20:45 -0800

    1.0.3: JS updates

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ const data = await fetch("/static.json").then((x) => x.json())
 let version = data.version
 
 document.getElementById("static").innerHTML = 'Version ' + version /**'·' */
+function makeCookie(cname, cvalue, days) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function initiate() {
+  console.log('Start')
+}
