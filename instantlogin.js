c8b03a0271e14af60a3fb69b19443e2108a0149e
Preston Kwei <prestonkwei@gmail.com>
2024-06-22 08:35:05 -0700

    2.0.1: SSO with QR code implementation

--- instantlogin.js
+++ instantlogin.js
@@ -10,10 +10,16 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig)
 let database = firebase.database()
 
+let currentUrl = window.location.href
+if (currentUrl.includes('%26')) {
+  let updatedUrl = currentUrl.replace(/%26/g, '&')
+  window.location.href = updatedUrl
+}
+
 const urlParams = new URLSearchParams(window.location.search)
 
 let loginHint = urlParams.get('loginHint')
-let ssoId = urlParams.get('ssoIds')
+let ssoId = urlParams.get('ssoId')
 console.log(loginHint)
 console.log(ssoId)
 if (loginHint == null || loginHint == '') {
@@ -22,4 +28,110 @@ if (loginHint == null || loginHint == '') {
     document.querySelector('#instant-lander').style.display = 'none'
   }
 }
-// KNOWN ISSUE: when visiting this page, the url params dont register "%26" as a &, so loginHint will always return null. To do: fix it so that it either switches %26 to a & and then reloads the page with the new url params, or qr code creation fix to encode unicode?
+
+let ssoConfirmed = false
+let loginHintConfirmed = false
+let userId = null
+async function checkDatabase() {
+  try {
+    let snapshot = await database.ref(`sso/${ssoId}/`).once('value')
+    const data = snapshot.val()
+    let email = data?.email || null
+    console.log(email)
+    if (email != null && email == loginHint) {
+      loginHintConfirmed = true
+    } else {
+      document.querySelector('#instant-missingperms').style.display = 'block'
+      document.querySelector('#instant-lander').style.display = 'none'
+    }
+
+    snapshot = await database.ref(`sso/${ssoId}/`).once('value')
+    const sso = data?.ssoId || null
+    console.log(sso)
+    if (sso != null && sso == ssoId) {
+      ssoConfirmed = true
+    } else {
+      document.querySelector('#instant-missingperms').style.display = 'block'
+      document.querySelector('#instant-lander').style.display = 'none'
+    }
+    snapshot = await database.ref(`sso/${ssoId}/`).once('value')
+    const accid = data?.accUuid || null
+    console.log(accid)
+    userId = accid
+    snapshot = await database.ref(`sso/${ssoId}/`).once('value')
+    const expiration = data?.expires || null
+    console.log(expiration)
+    console.log(Date.now())
+    if ((expiration - Date.now()) > 0) {
+      if (ssoConfirmed) {
+        console.log('ssoConfirmed')
+        if (loginHintConfirmed) {
+          console.log('loginHintConfirmed')
+          // QR CODE AUTHENTICATED -- PROCEED WITH LOGIN, REDIRECT AND AUTHTOKEN
+          document.querySelector('#instant-confirm-wrapper').style.display = 'flex'
+          document.querySelector('#instant-lander').style.display = 'none'
+          document.getElementById('instant-confirm-button').classList.add('buttonshade')
+          setTimeout(() => {
+            document.getElementById('instant-confirm-button').disabled = false
+            document.getElementById('instant-confirm-button').style.cursor = 'pointer'
+          }, '3000')
+        }
+      }
+    } else {
+      document.querySelector('#instant-missingperms').style.display = 'block'
+      document.querySelector('#instant-lander').style.display = 'none'
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+}
+
+checkDatabase()
+let d = new Date().toString().replace(/ /g, '').replace(/GMT/g, 'UTC')
+let ipAuthToken = ''
+let emailAuthToken = ''
+let tsAuthToken = ''
+let statusAuthToken = true
+let uuidAuthToken = self.crypto.randomUUID()
+
+async function authToken(email) {
+  console.log('Creating Auth Token')
+  let tsAuthToken = Math.floor(Date.now() / 1000)
+  emailAuthToken = email
+  let expiration = new Date(Date.now() + 6 * 60 * 60 * 1000)
+  try {
+    const response = await fetch('https://api.ipify.org?format=json')
+    const data = await response.json()
+    ipAuthToken = data.ip
+    console.log(ipAuthToken)
+    await database.ref('auth-tokens/' + uuidAuthToken).update({
+      id: uuidAuthToken,
+      status: statusAuthToken,
+      ts: tsAuthToken,
+      email: emailAuthToken,
+      ip: ipAuthToken,
+    })
+    document.cookie = 'auth-token=id=' + uuidAuthToken + '&ip=' + ipAuthToken + '&ts=' + tsAuthToken + '&status=' + statusAuthToken + '&e=' + emailAuthToken + '; expires=' + expiration.toUTCString()
+    console.log('Auth Token Created!')
+  } catch (error) {
+    console.error('Error creating auth token:', error)
+    ipAuthToken = 'ERROR'
+    throw error
+  }
+}
+
+document.querySelector('#instant-confirm-button').addEventListener('click', () => {
+  console.log('Confirm Login')
+  document.querySelector('#instant-confirm').style.display = 'none'
+  document.querySelector('#confirm-loader').style.display = 'block'
+  authToken(loginHint.replace(/_/g, '@').replace(/,/g, '.')).then(() => {
+    let date = new Date()
+    date.setTime(date.getTime() + (0.1666666 * 24 * 60 * 60 * 1000))
+    let expires = 'expires=' + date.toUTCString()
+    document.cookie = 'loggedin=true; userId=' + userId + '; ' + expires  
+    window.location.replace(`/account.html?id=${userId}&e=${loginHint}&s=true&ts=${Date.now()}`)
+  }).catch(error => {
+    console.error('Login failed:', error)
+    // Handle login failure, e.g., show an error message to the user
+  })
+})
